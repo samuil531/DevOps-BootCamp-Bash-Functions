981e8c7275045b972e328f660137723717e24e2d
samuil531 <108720085+samuil531@users.noreply.github.com>
2022-07-11 19:51:40 +0300

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
-# Put your code here
+pow () {
+    result=$(($1**$2))
+    echo $result
+}
+
+shortest () {
+    total=$#
+    arguments=( $@ )
+    shortest_string_lenght=${#arguments[0]}
+    shortest_string_number=0
+
+    for (( i=0; i<$total; i++ ))
+    do
+        current_string=${arguments[$i]}
+        if [[ ${#current_string} < $shortest_string_lenght ]]
+        then
+            shortest_string_lenght=${#current_string}
+        fi
+    done
+
+    for (( i=0; i<$total; i++ ))
+    do
+        current_string=${arguments[$i]}
+        if [[ ${#current_string} == $shortest_string_lenght ]]
+        then
+            echo $current_string
+        fi
+    done
+}
+
+print_log () {
+    now=$(date +%F" "%R)
+    echo "["$now"]" $1
+}
